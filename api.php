d0243f639b3b1c1f3803cde3f627fa9229c2bd8e
TehMuffinMoo <mat.cox@tmmn.uk>
2024-12-30 01:14:37 +0000

    ansible updates

--- api.php
+++ api.php
@@ -14,7 +14,7 @@ $app->get('/plugin/cmdb/settings', function ($request, $response, $args) {
 // Get CMDB Layout (Used to build the tables & form)
 $app->get('/plugin/cmdb/layout', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-READ'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-READ'] ?? null)) {
         $cmdbPlugin->api->setAPIResponseData($cmdbPlugin->getColumnsAndSections());
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -28,7 +28,7 @@ $app->get('/plugin/cmdb/layout', function ($request, $response, $args) {
 // Get CMDB Records
 $app->get('/plugin/cmdb/records', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-READ'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-READ'] ?? null)) {
         $cmdbPlugin->api->setAPIResponseData($cmdbPlugin->getAllRecords());
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -40,7 +40,7 @@ $app->get('/plugin/cmdb/records', function ($request, $response, $args) {
 // Create New CMDB Record
 $app->post('/plugin/cmdb/record', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Create the CMDB record with the submitted data
         $cmdbPlugin->newRecord($data);
@@ -54,7 +54,7 @@ $app->post('/plugin/cmdb/record', function ($request, $response, $args) {
 // Update CMDB Record
 $app->patch('/plugin/cmdb/record/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Update the CMDB record values with the submitted data
         $cmdbPlugin->updateRecord($args['id'], $data);
@@ -69,7 +69,7 @@ $app->patch('/plugin/cmdb/record/{id}', function ($request, $response, $args) {
 // Delete CMDB Record
 $app->delete('/plugin/cmdb/record/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-WRITE'] ?? null)) {
         // Delete the CMDB Record
         $cmdbPlugin->deleteRecord($args['id']);
     }
@@ -85,7 +85,7 @@ $app->delete('/plugin/cmdb/record/{id}', function ($request, $response, $args) {
 // Get CMDB Sections
 $app->get('/plugin/cmdb/sections', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
         $cmdbPlugin->api->setAPIResponseData($cmdbPlugin->getSections());
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -97,7 +97,7 @@ $app->get('/plugin/cmdb/sections', function ($request, $response, $args) {
 // Create New CMDB Section
 $app->post('/plugin/cmdb/sections', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Create the CMDB Section with the submitted data
 		if (isset($data["name"])) {
@@ -113,7 +113,7 @@ $app->post('/plugin/cmdb/sections', function ($request, $response, $args) {
 // Update CMDB Section
 $app->patch('/plugin/cmdb/section/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Create the CMDB Section with the submitted data
 		if (isset($data["name"])) {
@@ -129,7 +129,7 @@ $app->patch('/plugin/cmdb/section/{id}', function ($request, $response, $args) {
 // Delete CMDB Section
 $app->delete('/plugin/cmdb/section/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$cmdbPlugin->removeSection($args["id"]);
     }
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -143,7 +143,7 @@ $app->delete('/plugin/cmdb/section/{id}', function ($request, $response, $args) 
 // Get CMDB Columns
 $app->get('/plugin/cmdb/columns', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$data = $request->getQueryParams();
 		if (isset($data['section'])) {
 			$cmdbPlugin->api->setAPIResponseData($cmdbPlugin->getColumnDefinitionBySectionId($data['section']));
@@ -161,7 +161,7 @@ $app->get('/plugin/cmdb/columns', function ($request, $response, $args) {
 // Create New CMDB Column
 $app->post('/plugin/cmdb/columns', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Create the CMDB Section with the submitted data
 		$name = $data['name'] ?? null;
@@ -186,7 +186,7 @@ $app->post('/plugin/cmdb/columns', function ($request, $response, $args) {
 // Update CMDB Column
 $app->patch('/plugin/cmdb/column/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
         $data = $cmdbPlugin->api->getAPIRequestData($request);
         // Create the CMDB Section with the submitted data
 		if (isset($data)) {
@@ -202,7 +202,7 @@ $app->patch('/plugin/cmdb/column/{id}', function ($request, $response, $args) {
 // Update CMDB Column Weight
 $app->patch('/plugin/cmdb/column/{id}/weight', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$data = $cmdbPlugin->api->getAPIRequestData($request);
 		if (isset($data['weight'])) {
 			if ($cmdbPlugin->updateColumnWeight($args['id'],$data['weight'])) {
@@ -223,7 +223,7 @@ $app->patch('/plugin/cmdb/column/{id}/weight', function ($request, $response, $a
 // Delete CMDB Section
 $app->delete('/plugin/cmdb/column/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$cmdbPlugin->removeColumnDefinition($args["id"]);
     }
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -235,7 +235,7 @@ $app->delete('/plugin/cmdb/column/{id}', function ($request, $response, $args) {
 // Update CMDB Section Weight
 $app->patch('/plugin/cmdb/section/{id}/weight', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$data = $cmdbPlugin->api->getAPIRequestData($request);
 		if (isset($data['weight'])) {
 			if ($cmdbPlugin->updateSectionWeight($args['id'],$data['weight'])) {
@@ -258,7 +258,7 @@ $app->patch('/plugin/cmdb/section/{id}/weight', function ($request, $response, $
 // Check If DB Rebuild Required
 $app->get('/plugin/cmdb/dbRebuild', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		if ($cmdbPlugin->rebuildRequired()) {
 			$cmdbPlugin->api->setAPIResponse('Warning','Database rebuild required to remove old columns.');
 		}
@@ -272,7 +272,7 @@ $app->get('/plugin/cmdb/dbRebuild', function ($request, $response, $args) {
 // Check If DB Rebuild Required
 $app->post('/plugin/cmdb/dbRebuild/initiate', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPlugin();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$cmdbPlugin->updateCMDBColumns(true);
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -285,7 +285,7 @@ $app->post('/plugin/cmdb/dbRebuild/initiate', function ($request, $response, $ar
 //  Return list of Ansible Labels
 $app->get('/plugin/cmdb/ansible/labels', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPluginAnsible();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-ADMIN'] ?? null)) {
 		$cmdbPlugin->GetAnsibleLabels();
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -297,7 +297,7 @@ $app->get('/plugin/cmdb/ansible/labels', function ($request, $response, $args) {
 //  Return list of Ansible Job Templates
 $app->get('/plugin/cmdb/ansible/templates', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPluginAnsible();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'] ?? null)) {
 		$data = $request->getQueryParams();
 		$Label = $data['label'] ?? null;
 		$Id = $data['id'] ?? null;
@@ -312,7 +312,7 @@ $app->get('/plugin/cmdb/ansible/templates', function ($request, $response, $args
 //  Return list of Ansible Jobs
 $app->get('/plugin/cmdb/ansible/jobs', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPluginAnsible();
-	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'])) {
+	if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'] ?? null)) {
 		$cmdbPlugin->GetAnsibleJobs();
 	}
 	$response->getBody()->write(jsonE($GLOBALS['api']));
@@ -324,7 +324,7 @@ $app->get('/plugin/cmdb/ansible/jobs', function ($request, $response, $args) {
 // Submit Ansible Job
 $app->post('/plugin/cmdb/ansible/job/{id}', function ($request, $response, $args) {
 	$cmdbPlugin = new cmdbPluginAnsible();
-    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'])) {
+    if ($cmdbPlugin->auth->checkAccess($cmdbPlugin->config->get('Plugins','cmdb')['ACL-JOB'] ?? null)) {
 		$data = $cmdbPlugin->api->getAPIRequestData($request);
 		$DataArray = array(
 			"extra_vars" => array()
